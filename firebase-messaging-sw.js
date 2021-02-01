894f47d8464883b411b27a4b28ef8f0c382df0d5
a-s-k-u <arun-sree-kumar@outlook.com>
2021-02-01 15:42:37 +1100

    interim update

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js');
+firebase.initializeApp({
+    apiKey: "AIzaSyAOczQfKCpPdSvq59Do35cX5CoPkAW4rj8",
+    authDomain: "kochuveedu-1e448.firebaseapp.com",
+    databaseURL: "https://kochuveedu-1e448.firebaseio.com",
+    projectId: "kochuveedu-1e448",
+    storageBucket: "kochuveedu-1e448.appspot.com",
+    messagingSenderId: "567983583151",
+    appId: "1:567983583151:web:e41abe9dcb75fb438c095a",
+    measurementId: "G-5FPCM4GB60"
+});
+const messaging = firebase.messaging();
